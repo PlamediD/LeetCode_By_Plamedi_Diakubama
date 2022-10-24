9f954f1d055d11ffcd58d537ed79f21dd6d5276a
Plamedi K Diakubama <87151146+PlamediD@users.noreply.github.com>
2022-10-24 15:36:48 -0700

    Time: 231 ms (71.29%), Space: 43.3 MB (38.17%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cs
@@ -1,11 +1,6 @@
 public class Solution {
     public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
-        int max=0; 
-        for (int i=0; i<candies.Length; i++){
-            if(candies[i]> max){
-                max=candies[i]; 
-            }
-        }
+        int max=candies.Max();
         List<bool> result = new List<bool>();
         
         for(int i=0; i<candies.Length; i++){
